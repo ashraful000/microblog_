727df9d59ae0b29c8e3994c1454105025dca0293
Administrator <admin@iMac-3.local>
2019-06-27 02:26:29 +0600

    week 2

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,93 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+rom app import app
+from flask import render_template, flash, redirect, url_for, request
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from flask_login import current_user, login_user, login_required
+from app.models import User, Post
+from flask_login import logout_user
+from werkzeug.urls import url_parse
+from app import db
+from datetime import datetime
 
 
-@app.route('/')
-@app.route('/index')
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+@login_required
 def index():
-    user = {'username': ‘ashraful’}
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your Post is now live!')
+        return redirect(url_for('index'))
     posts = [
-        {
-            'author': {'username': ’saka’},
-            'body': 'Beautiful day in BangladeshPortland!'
-        },
-        {
-            'author': {'username': ’Saidur’},
-            'body': 'The Avengers movie was so cool!'
-        }
+            {
+                'author': {'username': 'John'},
+                'body': 'Beautiful day in Portland!'
+            },
+            {
+                'author': {'username': 'Susan'},
+                'body': 'The Avengers movie was so cool!'
+            }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    posts = current_user.followed_posts().all()
+    return render_template("index.html", title='Home page',form=form, posts=posts)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.html',  title='Sign In', form=form)
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title = 'Sign In', form=form)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('user', username=current_user.username))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
