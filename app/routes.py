cf928b6d5724e075230d6aa8af783a150dc534bb
sakhawath-hossain <51797497+sakhawath-hossain@users.noreply.github.com>
2019-07-03 23:54:29 +0600

    user database

--- app/routes.py
+++ app/routes.py
@@ -85,7 +85,33 @@ def user(username):
     ]
     return render_template('user.html', user=user, posts=posts)
 
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You can not follow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash('You are following {}!'.format(username))
+    return redirect(url_for('user', username=username))
 
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You can not unfollow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('You are not following {}!'.format(username))
+    return redirect(url_for('user', username=username))
 
 @app.route('/logout')
 def logout():
