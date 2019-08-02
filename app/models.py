05f27cfa23e589e467d2dcfc452a334ba00a590d
ashraful000 <52004817+ashraful000@users.noreply.github.com>
2019-08-02 22:54:23 +0600

    Update models.py

--- app/models.py
+++ app/models.py
@@ -53,6 +53,19 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest,size)
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)    
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
